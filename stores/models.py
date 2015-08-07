c1022b94d37560e7fc18e413a04b676f261ba0e2
bustta <bustta80980@gmail.com>
2015-08-07 23:05:20 +0800

    add store model

--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    category = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    area = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    post_code = models.IntegerField()
+    telephone = models.CharField(max_length=30)
+    website = models.SlugField()
+
